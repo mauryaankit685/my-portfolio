5805706deaaa68d2a19c36d3f040f7c3966308fd
Ankit Maurya <mauryaankit685@gmail.com>
2026-03-05 19:52:43 +0530

    changes v05 updateded script for deploy

--- backend-python/app.py
+++ backend-python/app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from rembg import remove
 from PIL import Image
 import io
-import os
+
 app = Flask(__name__)
 CORS(app)
 
@@ -24,8 +24,3 @@ def remove_bg():
     img_io.seek(0)
 
     return send_file(img_io, mimetype="image/png")
-
-
-if __name__ == "__main__":
-    port=int (os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0", port=port)
