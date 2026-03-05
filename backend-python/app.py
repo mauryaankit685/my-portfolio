872d4db501ee17c46bc02054c673cf510907ecb4
Ankit Maurya <mauryaankit685@gmail.com>
2026-03-05 21:30:37 +0530

    changes v08 updateded script for deploy

--- backend-python/app.py
+++ backend-python/app.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from rembg import new_session, remove
+from rembg import remove
 from PIL import Image
 import io
 
 app = Flask(__name__)
 CORS(app)
 
-# Load model only once when server starts
-session = new_session("u2netp")
+@app.route("/")
+def home():
+    return "Background Removal API Running"
 
 @app.route("/remove-bg", methods=["POST"])
 def remove_bg():
@@ -20,18 +21,15 @@ def remove_bg():
     try:
         input_image = Image.open(file.stream).convert("RGBA")
 
-        output = remove(input_image, session=session)
+        # simpler remove call
+        output = remove(input_image)
 
         img_io = io.BytesIO()
         output.save(img_io, "PNG")
         img_io.seek(0)
 
-        return send_file(
-            img_io,
-            mimetype="image/png",
-            as_attachment=False
-        )
+        return send_file(img_io, mimetype="image/png")
 
     except Exception as e:
-        print("ERROR:", e)
+        print(e)
         return {"error": str(e)}, 500
